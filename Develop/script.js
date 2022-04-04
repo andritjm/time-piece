f1a37c3526de61a905ffd2bb7cce8cff7dd80aba
andritjm <jamesandritsch@yahoo.com>
2022-04-03 23:39:13 -0500

    added to javascript, fixed accidental copy of container divs

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,25 @@
-var rightNow = moment().format("dddd, MMMM DD, YYYY");
+var currentTime = moment().format("dddd, MMMM DD, YYYY");
 var time = moment().hours();
 
 // show current time in header
-$('#currentDay').text(rightNow);
+$('#currentDay').text(currentTime);
 
 // change box color based on current time
-$("#9 .task").val(localStorage.getItem("9"));
-$("#10 .task").val(localStorage.getItem("10"));
-$("#11 .task").val(localStorage.getItem("11"));
-$("#12 .task").val(localStorage.getItem("12"));
-$("#13 .task").val(localStorage.getItem("13"));
-$("#14 .task").val(localStorage.getItem("14"));
-$("#15 .task").val(localStorage.getItem("15"));
-$("#16 .task").val(localStorage.getItem("16"));
-$("#17 .task").val(localStorage.getItem("17"));
+
+// send text to storage
+$(".saveBtn").on("click", function () {
+    var assignment = $(this).siblings(".assignment").val();
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, assignment);
+})
+
+// retrieve text
+$("#9 .assignment").val(localStorage.getItem("9"));
+$("#10 .assignment").val(localStorage.getItem("10"));
+$("#11 .assignment").val(localStorage.getItem("11"));
+$("#12 .assignment").val(localStorage.getItem("12"));
+$("#13 .assignment").val(localStorage.getItem("13"));
+$("#14 .assignment").val(localStorage.getItem("14"));
+$("#15 .assignment").val(localStorage.getItem("15"));
+$("#16 .assignment").val(localStorage.getItem("16"));
+$("#17 .assignment").val(localStorage.getItem("17"));
